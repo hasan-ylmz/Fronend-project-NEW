7f667aa10a45f086a1d5354d5b776f0fb4898551
Hasan <hasanktu98@outlook.com>
2022-07-11 20:57:07 +0300

    ajax json country file changed

--- Html-Css-Bootstrap-Javascript/Javascript_Notes/JS_API_2/ajax_2/ajax_2.js
+++ Html-Css-Bootstrap-Javascript/Javascript_Notes/JS_API_2/ajax_2/ajax_2.js
@@ -1,39 +1,42 @@
-function displayCountry(country) {
-    const request = new XMLHttpRequest();
+/* Json */
 
-    request.open('GET', 'https://restcountries.com/v3.1/name/'+ country);
+function displayCountry(country){
+    const request = new XMLHttpRequest();
+    request.open("get","https://restcountries.com/v3.1/name/"+country);
     request.send();
 
-    // async
+    request.addEventListener("load",JsonFunction)
 
-    request.addEventListener('load', function() {
+    function JsonFunction(){
+    // String to json
         const data = JSON.parse(this.responseText);
-        console.log(data);            
+        console.log(data[0]);
         setCountry(data[0]);
-    });
-   }
+    }
+}
 
-   function setCountry(data) {
-       const html = `
-            <div class="col-3">
-                <div class="card h-100">
-                    <img src="${data.flags.png}" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">${data.name.common}</h5>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Population: ${(data.population / 1000000).toFixed(1)} </li>
-                        <li class="list-group-item">Capital: ${data.capital[0]} </li>
-                        <li class="list-group-item">Language: ${Object.values(data.languages)} </li>
-                    </ul>
-                </div>
+function setCountry(data){
+    const html = 
+     `       
+        <div class="col-3">
+            <div class="card h-100">
+            <img src="${data.flags.png}" alt="" class="img-top">
+            <div class="card-body">
+                <h5 class="card-title">Name : ${data.name.common}</h5>
+                <p class="card-text"></p>
             </div>
-       `;
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Capital : ${data.capital}</li>
+                <li class="list-group-item">Language : ${Object.values(data.languages)}</li>
+                <li class="list-group-item">Population : ${data.population}</li>
+                <li class="list-group-item">Region : ${data.region}</li>
+            </ul>
+        </div>
+     </div>
+    `
+    document.querySelector(".container .row").insertAdjacentHTML("beforeend",html)
+};
 
-       document.querySelector(".container .row").insertAdjacentHTML("beforeend", html);
-   }
-
-
-   displayCountry("turkey");
-   displayCountry("czech");
-   displayCountry("Poland")
+displayCountry("poland");
+displayCountry("turkey");
+displayCountry("United States");
